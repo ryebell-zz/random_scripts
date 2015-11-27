5df8dca87a2ed1c233aec5fee0d7746ff673da43
Ryan Belisle <ryan@ryebell.me>
2015-11-26 21:27:19 -0500

    add domain status output to primary TLDs

--- modules/dnsplus.sh
+++ modules/dnsplus.sh
@@ -7,15 +7,16 @@ echo "@ (Root A Record):\n$(echo $IP | sed 's/^/ /')";
 echo "MX:\n$(dig +short mx $DOMAIN | sed 's/^/ /')";
 echo "NS:\n$(dig +short ns $DOMAIN | sed 's/^/ /')";
 echo "rDNS:\n$(dig +short -x $IP | sed 's/^/ /')";
+echo "Domain Information:"
 case $DOMAIN in 
     *.com | *.net)
-        whois $DOMAIN | grep  -ie "expiration date:" -ie "registrar:" | tail -2
+        whois $DOMAIN | grep  -ie "expiration date:" -ie "registrar:" -ie "domain status:" | tail -n6
         ;;
     *.co.uk)
-        whois $DOMAIN | sed "s/^[ \t]*//" | grep  -ie "expiry date:" -ie "registrar:" -A1 | sed 'N;s/\n/ /' | awk '{gsub("-- ", "");print}' | sed '/Last updated:/d'
+        whois $DOMAIN | sed "s/^[ \t]*//" | grep  -ie "expiry date:" -ie "registration status" -ie "registrar:" -A1 | sed 'N;s/\n/ /' | awk '{gsub("-- ", "");print}' | sed '/Last updated:/d' 
         ;;
     *.org)
-        whois $DOMAIN | grep  -ie "registry expiry date:" -ie "sponsoring registrar:" | tail -2
+        whois $DOMAIN | grep  -ie "registry expiry date:" -ie "sponsoring registrar:" -ie "domain status:" 
         ;;
     *.edu)
         whois $DOMAIN | grep  -i "domain expires:" | sed 's/ \{2,\}/ /g' && echo "Registrar: EDUCAUSE"
