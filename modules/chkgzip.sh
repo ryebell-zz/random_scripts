3d2f82b7bfdb2aed579f5663b0264d4f9d031749
Ryan Belisle <ryan@ryebell.me>
2015-11-27 21:02:14 -0500

    add URL parsing

--- modules/chkgzip.sh
+++ modules/chkgzip.sh
@@ -3,4 +3,9 @@
 
 test -z "$1" && echo "Usage: chkgzip domain.com" && return;
 domain=$1;
+    if ! [[ $domain =~ ^https?:// ]]; then
+        domain="http://${domain}";
+    fi;
+
+domain=$1;
 curl -sI -H 'Accept-Encoding: gzip,deflate' ${domain} | grep -i "content-encoding";
