83d5888e8e04cf85f2179135f3154f1ecf79c382
Ryan Belisle <ryan@ryebell.me>
2016-01-29 09:06:15 -0500

    update whois for ip and remove chksha

--- modules/chksha.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# tool for checking signature algorithm for installed SSL certificate remotely
-test -z "$1" && echo "Usage: chksha domain.com" && return;
-domain=$1;
-openssl s_client -connect ${domain}:443 < /dev/null 2> /dev/null | openssl x509 -text -in /dev/stdin | grep "sha";
